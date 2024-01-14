032e024854ee33a430457d10b07f6b508a34e24f
robert-chheav <robert.chheav@gmail.com>
2024-01-14 16:26:34 +0100

    make player name editable

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Player = ({name, symbol}) => {
+const Player = ({ name, symbol }) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleClick = () => {
+    setIsEditing(true);
+  };
+
+  let playerName = <span className="player-name">{name}</span>;
+
+  if (isEditing) {
+    playerName = <input type="text" required />;
+  }
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {playerName}
         <span className="player-symbol">{symbol}</span>
-        <button>Edit</button>
+        <button onClick={handleClick}>Edit</button>
       </span>
     </li>
   );
