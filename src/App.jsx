948e58af38e895dd8a1ba7d901a37db0824b6563
robert-chheav <robert.chheav@gmail.com>
2024-01-14 16:08:56 +0100

    payer name added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-
 function App() {
-
-
   return (
-    <p>coming soon...</p>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">Robert</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">Eszert</span>
+            <span className="player-symbol">O</span>
+          </li>
+        </ol>
+        GAME BOARD
+      </div>
+      LOG
+    </main>
+  );
 }
 
-export default App
+export default App;
