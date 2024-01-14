05d7d6d1cf26eec785fe1047b8109d320686917d
robert-chheav <robert.chheav@gmail.com>
2024-01-14 16:34:37 +0100

    config boelan save/edit

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -4,20 +4,21 @@ const Player = ({ name, symbol }) => {
   const [isEditing, setIsEditing] = useState(false);
 
   const handleClick = () => {
-    setIsEditing(true);
+    setIsEditing(!isEditing);
   };
 
   let playerName = <span className="player-name">{name}</span>;
-
+let btnCaption ='Edit'
   if (isEditing) {
-    playerName = <input type="text" required />;
+    playerName = <input type="text" required value={name} />;
+    btnCaption = 'Save'
   }
   return (
     <li>
       <span className="player">
         {playerName}
         <span className="player-symbol">{symbol}</span>
-        <button onClick={handleClick}>Edit</button>
+        <button onClick={handleClick}>{btnCaption}</button>
       </span>
     </li>
   );
