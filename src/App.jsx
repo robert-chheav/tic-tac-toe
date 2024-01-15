e20ae29540bd3f1c71cf89d3fb24d49360e192d0
robert-chheav <robert.chheav@gmail.com>
2024-01-15 16:54:32 +0100

    importing all winning conditions

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import { useState } from "react";
 import Log from "./components/Log";
+import { WINNING_COMBINATIONS } from "./winning-combinations";
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = "X";
   if (gameTurns.length > 0 && gameTurns[0].player === "X") {
